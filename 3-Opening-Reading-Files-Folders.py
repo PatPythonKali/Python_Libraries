f334804ad17b8933f59f4851e5f5100909fc5e6d
ubuntu <patpyhtonkali@gmail.com>
2021-09-04 05:01:30 +0800

    Update

---
+++ 3-Opening-Reading-Files-Folders.py
@@ -0,0 +1,9 @@
+with open('practice.txt', 'w+') as f:
+    f.write('This is a test string!')
+    f.close()
+
+import os
+# Get current Directory
+# print(os.getcwd())
+# List Directory
+# print(os.listdir('/'))
---
+++ 4-Shutil.py
@@ -0,0 +1,3 @@
+import shutil
+# Similar to mv command in UNIX
+shutil.move('practice.txt', '/home/ubuntu/PycharmProjects/Python_Libraries/Test-Dir/')
