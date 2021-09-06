ff8c1cc2dd1952d7c06557390fdb3f176357b37f
ubuntu-server <patpythonkali@gmail.com>
2021-09-07 01:54:46 +0800

    Update

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hello!")
+from collections import Counter
+
+# my_list = ['a', 'a', 'a', 'b', ]
+#
+# x = Counter(my_list)
+# print(x)
+
+word_list = 'aaaaaaaabbbbbbbbccc'
+
+for x in word_list:
+    print(Counter(x))
